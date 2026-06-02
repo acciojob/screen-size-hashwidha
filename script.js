ff0b8ed49bcfacbe4c3cde5e2107993369480722
hashwidha <147316086+hashwidha@users.noreply.github.com>
2026-06-02 16:27:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+let width= document.querySelector("#width");
+let height= document.querySelector("#height");
+window.addEventSelector("resize" function() {
+	width.innerhtml=window.innerWidth;
+	height.innerhtml=window.innerHeight;
+	
+})
